f0d14d88d583744ac0ad5719d7c12b24f038072a
Johann Hahn <wanische@gmail.com>
2024-06-29 23:07:39 +0200

    raylib submodule and gitignore

--- main.cpp
+++ main.cpp
@@ -1,2 +1,20 @@
 #include <iostream>
+#include "raylib/src/raylib.h"
+
+float window_width = 600;
+float window_height = 600;
+const char* window_title = "not doom";
+
+int main() {
+    std::cout << "hi\n";
+    InitWindow(window_width, window_height, window_title);
+
+    while(WindowShouldClose()) {
+	BeginDrawing();
+	ClearBackground(RED);
+	EndDrawing();
+    }
+    CloseWindow();
+    return 0;
+}
 
