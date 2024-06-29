3d1ceee3ec19b80ba59b89134303150ca71b5d20
Johann Hahn <wanische@gmail.com>
2024-06-29 23:19:24 +0200

    stuff

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
+#include <cstring>
 #include <iostream>
 #include "raylib/src/raylib.h"
 
-float window_width = 600;
-float window_height = 600;
-const char* window_title = "not doom";
+typedef uint64_t u64;
+
+constexpr float window_width = 600;
+constexpr float window_height = 600;
+constexpr const char* window_title = "not doom";
+constexpr u64 num_cols = 10;
+constexpr u64 num_rows = 10;
+
+Vector2 player_pos = {0, 0};
+Vector2 player_dir = {1, 1};
+
+bool level[num_rows * num_cols] = {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+};
+
+void draw_map() {
+    for
+}
 
 int main() {
     std::cout << "hi\n";
